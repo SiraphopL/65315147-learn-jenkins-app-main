311f8ed251248f11a44e1238582861e4af5fecbe
SiraphopL <pleum48695@gmail.com>
2025-03-24 21:01:09 +0700

    new all app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn Jenkins link', () => {
+test('renders greeting message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Jenkins/i);
-  expect(linkElement).toBeInTheDocument();
+  const greetingElement = screen.getByText(/สวัสดีครับ ผมชื่อสิรภพ/i);
+  expect(greetingElement).toBeInTheDocument();
 });
